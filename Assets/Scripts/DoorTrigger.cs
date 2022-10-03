295a534531041b0d34a5f5337ff72cd2a084dfd7
pyeons <105806835+pyeons@users.noreply.github.com>
2022-10-04 05:33:35 +0900

    vr 문열림 수정

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -4,31 +4,75 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
+    public bool doorState;
+    public bool doorSound;
+    public bool comState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        doorState = false;
+        doorSound = false;
+        comState = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (doorState && comState)
+        {
+            Complete();
+        }
     }
 
+    public void Complete()
+    {
+            this.transform.Translate(Vector3.up * Time.deltaTime);
+            
+            if (doorSound == false)
+            {
+                this.GetComponent<AudioSource>().Play();
+                doorSound = true;
+            }
+            if (this.transform.position.y > 500)
+            {
+                this.gameObject.SetActive(false);
+            }      
+    }
+
+    public void ComTrigger()
+    {
+        comState = true;
+    }
     void OnTriggerStay(Collider other)
     {
-        if ((other.tag == "Player" || other.tag == "PlayerMine") && Input.GetButtonDown("Interaction"))
+        if ((other.tag == "Player" || other.tag == "PlayerMine"))
         {
-            GameManagement.staticDoorTrigger = true;
+            doorState = true;
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player" || other.tag == "PlayerMine")
+        if ((other.tag == "Player" || other.tag == "PlayerMine"))
         {
-            GameManagement.staticDoorTrigger = false;
+            doorState = false;
         }
     }
+
+    //void OnTriggerStay(Collider other)
+    //{
+    //    if ((other.tag == "Player" || other.tag == "PlayerMine") && Input.GetButtonDown("Interaction"))
+    //    {
+    //        GameManagement.staticDoorTrigger = true;
+    //    }
+    //}
+
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if (other.tag == "Player" || other.tag == "PlayerMine")
+    //    {
+    //        GameManagement.staticDoorTrigger = false;
+    //    }
+    //}
 }
--- Assets/Scripts/PuzzleDirector.cs
+++ Assets/Scripts/PuzzleDirector.cs
@@ -12,6 +12,8 @@ public class PuzzleDirector : MonoBehaviourPunCallbacks
     GameObject door;
     public PhotonView PV;
     public bool doorSound = false;
+    public bool doorState;
+    GameObject DoorTrigger;
 
 
     // Start is called before the first frame update
@@ -19,20 +21,21 @@ public class PuzzleDirector : MonoBehaviourPunCallbacks
     {
         fireCount = 0;
         fireNumber = 10;
-        door = GameObject.Find("door");
+        //door = GameObject.Find("door");
 
         PV = photonView;
-
+        doorState = false;
+        DoorTrigger = GameObject.Find("door");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fireCount == fireNumber && GameManagement.staticDoorTrigger)
+        if (fireCount == fireNumber)
         {
             if(GameManagement.staticPlaymode == "soloplay" || GameManagement.staticPlaymode == null)
             {
-                Complete();
+                DoorTrigger.GetComponent<DoorTrigger>().ComTrigger();
             }
             else if(GameManagement.staticPlaymode == "multiplay")
             {
@@ -54,18 +57,35 @@ public class PuzzleDirector : MonoBehaviourPunCallbacks
     }
 
     //[PunRPC]
-    void Complete()
-    {
-        door.transform.Translate(Vector3.up * Time.deltaTime);
+    //void Complete()
+    //{
 
-        if(doorSound == false)
-        {
-            door.GetComponent<AudioSource>().Play();
-            doorSound = true;
-        }
-        if (door.transform.position.y > 500)
-        {
-            door.SetActive(false);
-        }
-    }
+    //    door.transform.Translate(Vector3.up * Time.deltaTime);
+        
+    //    if(doorSound == false)
+    //    {
+    //        door.GetComponent<AudioSource>().Play();
+    //        doorSound = true;
+    //    }
+    //    if (door.transform.position.y > 500)
+    //    {
+    //        door.SetActive(false);
+    //    }
+    //}
+
+    //void OnTriggerStay(Collider other)
+    //{
+    //    if ((other.tag == "Player" || other.tag == "PlayerMine"))
+    //    {
+    //        doorState = true;
+    //    }
+    //}
+
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if ((other.tag == "Player" || other.tag == "PlayerMine"))
+    //    {
+    //        doorState = false;
+    //    }
+    //}
 }
